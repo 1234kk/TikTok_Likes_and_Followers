db29f031370c1c34d6bb7af350cf587e5ec2e0e8
hackerrank <me@hackerrank.com>
2020-03-06 18:08:34 +0500

    Add Template code to HomeActivity's Fragments

---
+++ app/src/main/java/com/asadchattha/tiktoklikesandfollowers/fragments/earn/WatchVideosActivity.java
@@ -0,0 +1,16 @@
+package com.asadchattha.tiktoklikesandfollowers.fragments.earn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.asadchattha.tiktoklikesandfollowers.R;
+
+public class WatchVideosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_watch_videos);
+    }
+}
